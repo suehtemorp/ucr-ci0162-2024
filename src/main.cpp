b2327139a462271eb370526ae49f5c4b5891bbc4
Javier Alfredo Solano Saltachín <javier.solanosaltachin@ucr.ac.cr>
2024-08-29 12:25:47 -0600

    Small demo bump

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 // https://gist.github.com/cicanci/b54715298ab55dff2fbcd0ca3829d13b
 #include <SDL.h>
 
+#include <iostream>
+#include <random>
+#include <ECS/ECS.hpp>
+
 // Window size
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
@@ -11,8 +15,123 @@
 #define IMAGE_WIDTH 256
 #define IMAGE_HEIGHT 256
 
+class RankComponent : public Component { public: int rank; };
+
+class CounterService : public Service { public: int counter; };
+
+class RandIntService : public Service {
+    private:
+        std::mt19937* _generator = nullptr;
+        std::uniform_int_distribution<int> _distribution;
+
+        int _min = 0, _max = 0;
+
+    public:
+        RandIntService(int min, int max):
+            _min(min), _max(max), 
+            _distribution(std::uniform_int_distribution<int>(_min, _max)),
+            _generator(new std::mt19937()) 
+        {}
+
+        RandIntService(RandIntService&& other) {
+            _min = other._min;
+            _max = other._max;
+            _generator = other._generator;
+            _distribution = std::uniform_int_distribution<int>(_min, _max);
+
+            other._generator = nullptr;
+        }
+
+        RandIntService& operator=(RandIntService&& other) {
+            if (
+                _generator != other._generator 
+                && _generator != nullptr
+            ) {
+                delete _generator;
+            }
+
+            _min = other._min;
+            _max = other._max;
+            _generator = other._generator;
+            _distribution = std::uniform_int_distribution<int>(_min, _max);
+
+            other._generator = nullptr;
+            return *this;
+        }
+
+        int GetRandInt() {
+            return _distribution(*_generator);
+        }
+
+        ~RandIntService() {
+            if (_generator != nullptr) {
+                delete _generator;
+            }
+        }
+};
+
+using CountingECS = ECS<RankComponent>::WithServices<CounterService, RandIntService>;
+
+void CounterSystem(
+    std::tuple<const RankComponent&> components, 
+    std::tuple<CounterService&, CountingECS::ManagerService&> services
+) {
+    std::cout << "> Visiting component with rank " << std::get<0>(components).rank;
+    std::cout << " at count " << std::get<0>(services).counter++ << std::endl;;
+
+    if (std::get<0>(services).counter == 2048) {
+        std::get<1>(services).RequestStop();
+    }
+
+    return;
+}
+
+void RankShuffleSystem(
+    std::tuple<RankComponent&> components, 
+    std::tuple<RandIntService&> services
+) {
+    int oldRank = std::get<0>(components).rank; 
+    std::get<0>(components).rank = std::get<0>(services).GetRandInt();
+
+    std::cout << "> Promoted! From " << oldRank;
+    std::cout << " to " << std::get<0>(components).rank << std::endl;
+
+    return;
+}
+
+void TestECS() {
+    std::cout << "==== Testing ECS! ====" << std::endl;
+
+    std::cout << "Initializing ECS..." << std::endl ;
+    CountingECS ecs;
+
+    std::cout << "Adding entities..." << std::endl ;
+    ecs.AddEntity(RankComponent{.rank = 1});
+    ecs.AddEntity(RankComponent{.rank = 2});
+    ecs.AddEntity(RankComponent{.rank = 3});
+    ecs.AddEntity();
+
+    std::cout << "Adding systems..." << std::endl ;
+    ecs.AddSystem(CounterSystem);
+    ecs.AddSystem(RankShuffleSystem);
+
+    std::cout << "Adding service..." << std::endl ;
+    ecs.InstallService(CounterService{.counter = 0});
+    ecs.InstallService(RandIntService(10, 30));
+
+    std::cout << "Starting ECS..." << std::endl ;
+    ecs.Start();
+
+    std::cout << "Waiting for ECS stop..." << std::endl ;
+    ecs.AwaitStop();
+
+    std::cout << "==== All done! ====" << std::endl;
+}
+
 int main(int argc, char* args[])
 {
+    TestECS();
+
     // SDL initialisation
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
